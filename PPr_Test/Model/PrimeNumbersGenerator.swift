cb82865cfb62f147385eff30323904e389f2020f
MrBaseMax <shmaser@icloud.com>
2021-08-25 11:10:55 +0300

    primes detection optimized

--- PPr_Test/Model/PrimeNumbersGenerator.swift
+++ PPr_Test/Model/PrimeNumbersGenerator.swift
@@ -29,18 +29,18 @@ class PrimeNumbersGenerator: NumbersGenerator {
 	
 	
 	
-	// так себе алгоритм
+
 	private func isPrime( _ n: Int ) -> Bool {
 		
 		if n <= 1 { return false }
 		if n <= 3 { return true }
+		if n % 2 == 0 || n % 3 == 0 { return false }
 		
-		
-		var i = 2
+		var i = 5
 		while i*i <= n {
-			if n % i == 0 { return false }
-			
-			i += 1
+			if n % i == 0 || n % (i+2) == 0 { return false }
+
+			i += 6
 		}
 		
 		return true
